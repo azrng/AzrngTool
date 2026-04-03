2bb47a76c401a71225a0360f73b4613be0069c93
azrng <itzhangyunpeng@163.com>
2026-04-03 23:48:18 +0800

    fix: 融合数据库类型选择器主题

--- AzrngTools/Views/Database/Workbench/DatabaseTypeSelector.axaml.cs
+++ AzrngTools/Views/Database/Workbench/DatabaseTypeSelector.axaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
 using Avalonia.Controls;
-using Avalonia.Input;
 using Azrng.Core.Model;
-using AzrngTools.Models.Database;
 using AzrngTools.ViewModels.Database;
 
 namespace AzrngTools.Views.Database.Workbench;
@@ -15,49 +12,26 @@ public partial class DatabaseTypeSelector : UserControl
         SetupCardClickHandlers();
     }
 
-    /// <summary>
-    /// 设置卡片点击事件处理器
-    /// </summary>
     private void SetupCardClickHandlers()
     {
-        // 为每个数据库卡片添加 Tapped 事件处理
-        if (this.FindControl<Border>("SqlCard") is Border sqlCard)
-        {
-            sqlCard.Tapped += (s, e) => OnCardClicked(DatabaseType.SqlServer);
-        }
+        RegisterCardTap("SqlCard", DatabaseType.SqlServer);
+        RegisterCardTap("MySqlCard", DatabaseType.MySql);
+        RegisterCardTap("PostgreCard", DatabaseType.PostgresSql);
+        RegisterCardTap("SqliteCard", DatabaseType.Sqlite);
+        RegisterCardTap("OracleCard", DatabaseType.Oracle);
+        RegisterCardTap("DamengCard", DatabaseType.Dm);
+    }
 
-        if (this.FindControl<Border>("MySqlCard") is Border mySqlCard)
+    private void RegisterCardTap(string controlName, DatabaseType dbType)
+    {
+        if (this.FindControl<Control>(controlName) is { } card)
         {
-            mySqlCard.Tapped += (s, e) => OnCardClicked(DatabaseType.MySql);
-        }
-
-        if (this.FindControl<Border>("PostgreCard") is Border postgreCard)
-        {
-            postgreCard.Tapped += (s, e) => OnCardClicked(DatabaseType.PostgresSql);
-        }
-
-        if (this.FindControl<Border>("SqliteCard") is Border sqliteCard)
-        {
-            sqliteCard.Tapped += (s, e) => OnCardClicked(DatabaseType.Sqlite);
-        }
-
-        if (this.FindControl<Border>("OracleCard") is Border oracleCard)
-        {
-            oracleCard.Tapped += (s, e) => OnCardClicked(DatabaseType.Oracle);
-        }
-
-        if (this.FindControl<Border>("DamengCard") is Border damengCard)
-        {
-            damengCard.Tapped += (s, e) => OnCardClicked(DatabaseType.Dm);
+            card.Tapped += (_, _) => OnCardClicked(dbType);
         }
     }
 
-    /// <summary>
-    /// 卡片点击处理
-    /// </summary>
     private void OnCardClicked(DatabaseType dbType)
     {
-        // 通过 DataContext 调用 ViewModel 的命令
         if (DataContext is ConnectionDialogViewModel viewModel)
         {
             viewModel.SelectDatabaseTypeCommand?.Execute(dbType);
