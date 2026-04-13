d163144328f4df740ace1eea5f5d74085f8fec79
azrng <itzhangyunpeng@163.com>
2026-04-13 21:53:09 +0800

    fix(format): sync json editor input

--- AzrngTools/Views/Format/JsonPageView.axaml.cs
+++ AzrngTools/Views/Format/JsonPageView.axaml.cs
@@ -1,19 +1,32 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using AvaloniaEdit;
+using AzrngTools.ViewModels.Format;
 
 namespace AzrngTools.Views.Format
 {
     public partial class JsonPageView : ViewControlBase
     {
+        private readonly TextEditor? _textEditor;
+
         public JsonPageView()
         {
             InitializeComponent();
-            var textEditor = this.FindControl<TextEditor>("JsonText");
-            if (textEditor is not null)
+            _textEditor = this.FindControl<TextEditor>("JsonText");
+            if (_textEditor is not null)
             {
+                _textEditor.TextChanged += OnEditorTextChanged;
+
                 // todo ：格式还有问题
                 //textEditor.TextArea.TextView.ElementGenerators.Add(new TruncateLongLines());
+            }
+        }
+
+        private void OnEditorTextChanged(object? sender, EventArgs e)
+        {
+            if (DataContext is JsonPageViewModel viewModel && _textEditor is not null)
+            {
+                viewModel.Original = _textEditor.Text ?? string.Empty;
             }
         }
 
